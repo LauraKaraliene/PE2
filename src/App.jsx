6854113763e5bd01dbc6a0654f6612189ba2b658
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2025-06-25 20:26:14 +0200

    sets up project with vite, react and taillwind

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles/index.css";
+
+export default function App() {
+  return <div className="text-3xl text-red-500">Hello Tailwind!</div>;
+}
