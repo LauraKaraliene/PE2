ea5db1028469394a67013826dad4af340d4041f3
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2025-07-03 19:16:34 +0200

    adds custom theme, like fonts, colors, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./styles/index.css";
 
 export default function App() {
-  return <div className="text-3xl text-red-500">Hello Tailwind!</div>;
+  return (
+    <div className="min-h-screen bg-background-light flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-3xl text-primary">Custom Heading</h1>
+      <p className="text-secondary">Secondary color text</p>
+      <button className=" btn-primary text-white ">Primary Button</button>
+      <button className=" btn-secondary text-white ">Secondary Button</button>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        heading: ["Sora", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
