9e17325dba7ba35efb5853316fdb5e430bd567ef
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2025-07-14 14:42:51 +0200

    adds footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[var(--color-primary)] text-white text-center py-4">
+      <p className="text-sm">
+        <span className="inline-block mr-1">©</span> Copyright Laura Karaliene
+      </p>
+      <p className="text-sm">2025</p>
+      <p className="text-sm">Powered by Noroff API</p>
+    </footer>
+  );
+}
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
     <>
-      <Header />
-      <main className="pt-20 px-4">
-        <Outlet />
-      </main>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow pt-20 px-4">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
